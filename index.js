f5a7ac60fbc097213240caa09c795bccb4f3888a
jisdulanjana <93591453+jisdulanjana@users.noreply.github.com>
2022-12-15 01:07:25 +0530

    button

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
+// let startBtn = document.getElementById("start-btn")
+let messageEl = document.getElementById("message-el")
 let firstCard = 10
-let secondCard = 12
+let secondCard = 11
 let hasBlackJack = false
 let sum = firstCard + secondCard
 let isAlive =true
 let message = "" // empty string
-if (sum < 21) {
-  message = "Do you want to draw a new card? 🙂"
-} else if (sum === 21) {
-  message = "Wohoo! You've got Blackjack! 🥳"
-  hasBlackJack = true
-} else {
-  message = "You're out of the game! 😭"
-  isAlive = false
-}
 
-console.log(message)
-console.log(isAlive)
+
+function startGame() {
+    console.log("Game is starting...")
+    if (sum < 21) {
+        message = "Do you want to draw a new card? 🙂"
+      } else if (sum === 21) {
+        message = "Wohoo! You've got Blackjack! 🥳"
+        hasBlackJack = true
+      } else {
+        message = "You're out of the game! 😭"
+        isAlive = false
+      }
+      messageEl.textContent = message
+      console.log(message)
+      console.log(isAlive)
+}
